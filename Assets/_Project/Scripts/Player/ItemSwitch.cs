9ddb10604437abf361356b61714594ceae4e2d36
Carlos Alexandre Zgierski <carloszgierski@gmail.com>
2021-10-15 20:45:04 -0300

    Added some things

---
+++ Assets/_Project/Scripts/Player/ItemSwitch.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSwitch : MonoBehaviour
+{
+    [SerializeField] private GameObject[] items = default;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwitchItems();
+    }
+
+    void SwitchItems()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if(items[0].gameObject.activeSelf)
+            {
+                items[0].gameObject.SetActive(false);
+                items[1].gameObject.SetActive(true);
+            }
+            else
+            {
+                items[0].gameObject.SetActive(true);
+                items[1].gameObject.SetActive(false);
+            }
+        }
+    }
+}
--- Assets/_Project/Scripts/Player/PlayerAttack.cs
+++ Assets/_Project/Scripts/Player/PlayerAttack.cs
@@ -6,24 +6,34 @@ using Game.Entity;
 public class PlayerAttack : MonoBehaviour
 {
 
+    [SerializeField] Transform firePoint;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float bulletForce = 10f;
     private EntityData _entityData;
 
     private void Awake()
     {
-        _entityData = GetComponent<EntityData>();
+        _entityData = GetComponentInParent<EntityData>();
     }
     
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        firePoint.position = _entityData.LookDirection + transform.position;
+        float angle = Mathf.Atan2(_entityData.LookDirection.y, _entityData.LookDirection.x) * Mathf.Rad2Deg - 90f;
+        firePoint.eulerAngles = new Vector3(0, 0, angle);
+        if (Input.GetButtonDown("Fire1"))
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, _entityData.LookDirection);
-            if (hit)
-            {
-                var health = hit.collider.GetComponent<EntityHealth>();
-                health?.DealDamage(1f);
-            }
+            Shoot();
         }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); 
+        Rigidbody2D bulletRb =  bullet.GetComponent<Rigidbody2D>();
+
+        bulletRb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
     }
 
 }
